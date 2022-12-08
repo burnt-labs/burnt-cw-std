d2dc8a22409e97744f65e8439dc64e493328e2e8
peartes <kenny.fale.kf@gmail.com>
2022-12-08 19:24:31 +0100

    fix: remove redundant logic

--- redeemable/src/query.rs
+++ redeemable/src/query.rs
@@ -5,7 +5,6 @@ use crate::Redeemable;
 impl Redeemable<'_> {
     pub fn is_redeemed(&self, deps: &Deps, token_id: String) -> StdResult<bool> {
         let locked_items = self.locked_items.load(deps.storage)?;
-        locked_items.contains(&token_id);
         Ok(locked_items.contains(&token_id))
     }
 }
