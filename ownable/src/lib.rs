cb2f66f1f78751f59f5045b33847efdaa95a5e58
Ash <ash@burnt.com>
2023-07-11 11:25:15 -0700

    use set owner instead of saving directly

--- ownable/src/lib.rs
+++ ownable/src/lib.rs
@@ -72,6 +72,8 @@ impl<'a> Ownable<'a> {
     }
 
     pub fn set_owner(&self, deps: &mut DepsMut, addr: &Addr) -> StdResult<()> {
+        // validate Addr before saving
+        deps.api.addr_validate(addr.as_str())?;
         self.owner.save(deps.storage, addr)
     }
 }
@@ -111,7 +113,7 @@ impl<'a> Module for Ownable<'a> {
                 if info.sender != loaded_owner {
                     Err(Unauthorized {})
                 } else {
-                    self.owner.save(deps.storage, &owner).unwrap();
+                    self.set_owner(deps, &owner)?;
                     let resp = Response::new();
                     Ok(resp)
                 }
